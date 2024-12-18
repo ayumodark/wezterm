98d88d1668b6f499bdd9171131985e69e6785090
ayumodark <aayanmohammad@proton.me>
2024-12-18 17:00:53 +0600

    removed padding

--- .wezterm.lua
+++ .wezterm.lua
@@ -4,6 +4,13 @@ local config = wezterm.config_builder()
 
 config.enable_tab_bar = false
 
+config.window_padding = {
+	left = "0px",
+	right = "0px",
+	top = "0px",
+	bottom = "0px",
+}
+
 config.font = wezterm.font("JetBrainsMono Nerd Font")
 
 config.color_scheme = "nord"
